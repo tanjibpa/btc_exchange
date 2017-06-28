57a41b78f100b08413ada59f8302c8f5a5902ceb
Ikramul Alam <ikram.tanjib@gmail.com>
2017-06-28 23:14:21 +0600

    data writing on csv for gdax

--- app.py
+++ app.py
@@ -8,12 +8,16 @@ parser.add_argument("--exchange")
 args = parser.parse_args()
 
 if not os.path.exists('files/{}.csv'.format(args.exchange)):
-    f = open('{}.csv'.format(args.exchange), 'w')
+    f = open('files/{}.csv'.format(args.exchange), 'w')
     writer = csv.writer(f)
-    writer.writerow(['Time', 'Best price', 'Volume', 'Asks', 'Bids'])
-# else:
-
+    writer.writerow(['Time', 'Best price', 'Volume', 'Ask', 'Bid', 'Asks', 'Bids'])
+with open('files/{}.csv'.format(args.exchange), 'a') as f:
+    writer = csv.writer(f)
+    order_book = ex_gdax.get_order_book_by_id('BTC-USD')
+    ticker_data = ex_gdax.get_product_ticker_by_id('BTC-USD')
+    writer.writerow([ticker_data['time'], ticker_data['price'], ticker_data['volume'],
+                    ticker_data['ask'], ticker_data['bid'], order_book['asks'], order_book['bids']])
 
 if args.exchange == 'gdax':
-    print(ex_gdax.get_order_book('BTC-USD'))
+    print(ex_gdax.get_order_book_by_id('BTC-USD'))
     print(ex_gdax.get_product_ticker_by_id('BTC-USD'))
--- ex_gdax/ex_gdax.py
+++ ex_gdax/ex_gdax.py
@@ -1,11 +1,17 @@
 import gdax
 public_client = gdax.PublicClient()
 
-def get_order_book(product_id):
-    return public_client.get_product_order_book(product_id, level=1)
+
+def get_order_book_by_id(product_id):
+    data = public_client.get_product_order_book(product_id, level=1)
+    return {'asks': data['asks'][0],
+            'bids': data['bids'][0]}
+
 
 def get_product_ticker_by_id(product_id):
     data = public_client.get_product_ticker(product_id=product_id)
-    return data['price']
-
-# def get_
+    return {'time': data['time'],
+            'price': data['price'],
+            'volume': data['volume'],
+            'ask': data['ask'],
+            'bid': data['bid']}
